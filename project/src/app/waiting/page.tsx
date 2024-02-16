facdc9f20fb263f6c0a07e36545b3fe0ccbe1de8
JaimesJames <s.kaewkulsri1212@gmail.com>
2024-02-16 19:30:09 +0700

    test

--- project/src/app/waiting/page.tsx
+++ project/src/app/waiting/page.tsx
@@ -1,16 +1,4 @@
-"use client";
-import { signOut, useSession } from "next-auth/react";
-import { redirect } from "next/navigation";
-import { ref, onValue } from "firebase/database";
-import { db } from "./firebase";
-
-const waiting =():any =>{
-    const session = useSession({
-        required: true,
-        onUnauthenticated() {
-          redirect('/signin');
-        },
-      })
+const Waiting =():any =>{
     return(
         <>
             <h1>hii</h1>
@@ -18,6 +6,4 @@ const waiting =():any =>{
     )
 }
  
-export default waiting
- 
-waiting.auth = true
+export default Waiting
