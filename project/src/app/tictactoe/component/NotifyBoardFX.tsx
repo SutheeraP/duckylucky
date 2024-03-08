f50a212985d66df39322663f4458ca8b2edc41d3
PK <64070076@it.kmitl.ac.th>
2024-03-08 23:39:15 +0700

    update notifyboardFX

--- project/src/app/tictactoe/component/NotifyBoardFX.tsx
+++ project/src/app/tictactoe/component/NotifyBoardFX.tsx
@@ -6,13 +6,26 @@ const NotifyBoardFX = (props:any) => {
 
     return(
         <div className="flex bg-black bg-opacity-50 justify-center items-center w-screen z-30 h-screen absolute top-0">
-            <div className="flex flex-col gap-4">
-                <div className="font-bold text-xl flex justify-center bg-grayFX rounded-lg p-3 w-40">ช่องพิเศษทำงาน</div>
-                <div className="flex flex-col justify-center">
-                    <Image src={`${boardFXNotify.img}`} alt=""  width={240} height={280} className={`relative`} />
-                    <div className="text-white font-bold text-3xl flex justify-center -translate-y-8">{boardFXNotify.name}</div>
+            <div className="flex flex-col gap-4 w-screen p-4">
+                <div className="flex justify-center items-center">
+                    <div className={` bg-grayFX rounded-full py-2 px-4 font-bold md:text-md text-center`}>
+                        ช่องพิเศษทำงาน
+                    </div>
                 </div>
-                <div className="text-white font-bold text-xl flex justify-center -translate-y-8">{boardFXNotify.description}</div>
+                <div className="flex flex-col justify-center">
+                    <div className="flex justify-center">
+                        <Image src={`${boardFXNotify.img}`} alt=""  width={240} height={280}/>
+                    </div>
+                    <div className="flex justify-center -translate-y-8">
+                        <div className={`text-white font-bold md:text-3xl py-2 px-4 rounded-lg font-outline-12 absolute text-center`}>
+                            {boardFXNotify.name}
+                        </div> 
+                        <div className={`text-white font-bold md:text-3xl py-2 px-4 rounded-lg absolute text-center`}>
+                            {boardFXNotify.name}
+                        </div> 
+                    </div>
+                </div>
+                <div className={`text-white font-bold md:text-md flex justify-center md:translate-y-4 sm:translate-y-2 text-center`}>{boardFXNotify.description}</div>
             </div>
         </div>
     )
