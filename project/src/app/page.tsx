305a9119ef6f606b65fb6df7a3e6bc177ddfd863
JaimesJames <s.kaewkulsri1212@gmail.com>
2024-03-08 16:52:07 +0700

    update loading

--- project/src/app/page.tsx
+++ project/src/app/page.tsx
@@ -19,6 +19,7 @@ export default function Home() {
   })
 
   // base
+  const [status, setStatus] = useState('Loading...');
   const [email, setEmail] = useState("Loading...");
   const [username, setUsername] = useState("Loading...");
   const [img, setImg] = useState("/image/icon1.svg");
@@ -149,117 +150,124 @@ export default function Home() {
 
   return (
     <>
-      <div className=''>
-        <main className="min-h-screen items-center relative overflow-hidden">
-          <Background />
+      {currentUid != 'loading' ?
+        <div className=''>
+          <main className="min-h-screen items-center relative overflow-hidden">
+            <Background />
 
-          {showEdit ?
-            <div className="absolute h-full w-full flex z-20 bg-[#0005]">
-              <div className="container px-6 m-auto md:w-[600px]">
-                <div className=" bg-white ring-2 ring-black py-4 rounded-lg grid gap-8">
-                  <div>
-                    <div className="text-center mb-4">แก้ไขโปรไฟล์</div>
-                    <div className="bg-black w-full h-[2px]" />
+            {showEdit ?
+              <div className="absolute h-full w-full flex z-20 bg-[#0005]">
+                <div className="container px-6 m-auto md:w-[600px]">
+                  <div className=" bg-white ring-2 ring-black py-4 rounded-lg grid gap-8">
+                    <div>
+                      <div className="text-center mb-4">แก้ไขโปรไฟล์</div>
+                      <div className="bg-black w-full h-[2px]" />
+                    </div>
+                    <div className="px-4 md:px-8 pb-3 grid gap-7">
+                      <div className="grid grid-cols-3 md:grid-cols-6 gap-x-5 gap-y-5">
+                        {iconPath.map((path, index) => (
+
+                          <div key={index} className={`cursor-pointer rounded-full ${path == editIcon ? 'grayscale-0 ring-2 ring-black' : ''}`} onClick={() => { clickIcon(path) }}>
+                            <ImageComp path={path} />
+                          </div>
+
+                        ))}
+                      </div>
+                      <input type="text" className="border-black border-2 w-full rounded-lg md:w-[200px] mx-auto p-2" placeholder={username} onChange={(e) => { setEditName(e.target.value) }}></input>
+                      {feedback != "" ? <p className={`${color} bg-white py-1 rounded-md`} id="feedback-signup">{feedback}</p> : ""}
+                      <div className="grid grid-cols-2 gap-4">
+                        <button className="ring-2 ring-black rounded-lg bg-black text-white hover:bg-white hover:text-black py-2 px-6"
+                          onClick={() => saveEditProfile()}>บันทึก</button>
+                        <button className="ring-2 ring-black rounded-lg bg-black text-white hover:bg-white hover:text-black py-2 px-6"
+                          onClick={() => { setShowEdit(false); setFeedback('') }}>ยกเลิก</button>
+                      </div>
+                    </div>
                   </div>
-                  <div className="px-4 md:px-8 pb-3 grid gap-7">
-                    <div className="grid grid-cols-3 md:grid-cols-6 gap-x-5 gap-y-5">
-                      {iconPath.map((path, index) => (
+                </div>
+              </div> : null}
 
-                        <div key={index} className={`cursor-pointer rounded-full ${path == editIcon ? 'grayscale-0 ring-2 ring-black' : ''}`} onClick={() => { clickIcon(path) }}>
-                          <ImageComp path={path} />
+            {showLogout ?
+              <div className="absolute h-full w-full flex z-20 bg-[#0005]">
+                <div className="container px-4 m-auto md:w-96">
+                  <div className=" bg-white ring-2 ring-black px-4 py-8 rounded-lg grid gap-8">
+                    <div className="text-center">ออกจากระบบ</div>
+                    <div className="grid grid-cols-2 gap-8">
+                      <button className="ring-2 ring-black rounded-lg bg-white py-2 px-6"
+                        onClick={() => signOut()}>ยืนยัน</button>
+                      <button className="ring-2 ring-black rounded-lg bg-white py-2 px-6"
+                        onClick={() => { setShowLogout(false); setEditIcon(img) }}>ยกเลิก</button>
+                    </div>
+                  </div>
+                </div>
+              </div> : null
+            }
+
+            {invite == '' ? null :
+              <div className="absolute h-full w-full flex z-20 bg-[#0005]">
+                <div className=" m-auto bg-white ring-2 ring-black p-10 rounded-lg grid gap-8">
+                  <div className="text-center">ได้รับคำเชิญ !</div>
+                  <div className="grid grid-cols-2 gap-8">
+
+                    <button className="ring-2 ring-black rounded-lg bg-white py-2 px-6"
+                      onClick={() => { router.push(`/waitingroom?intend=${invite}`); removeInvite() }}>เข้าร่วม</button>
+                    <button className="ring-2 ring-black rounded-lg bg-white py-2 px-6"
+                      onClick={() => { setInvite(''); removeInvite() }}>ปฏิเสธ</button>
+
+                  </div>
+                </div>
+              </div>
+            }
+
+            <div className="min-h-screen flex">
+              <div className="container px-6 relative z-10 my-auto mx-auto">
+                <div className="">
+                  <div className="grid grid-cols-1 gap-8 mb-12 md:grid-cols-2">
+                    <div id="profile" className="ring-2 ring-black rounded-xl bg-white">
+                      <div className="px-3 py-2">
+                        <button onClick={() => { setShowEdit(true); }} className="w-5 mr-2">
+                          <ImageComp path='/image/icon/setting.svg' />
+                        </button>
+                        <button onClick={() => { setShowLogout(true) }} className="w-5">
+                          <ImageComp path='/image/icon/logout.svg' />
+                        </button>
+                      </div>
+
+                      <div className="w-full ring-black ring-1 "></div>
+
+                      <div className="grid grid-cols-2 md:grid-cols-1 p-6">
+                        <div className='md:w-2/5 md:mx-auto'>
+                          <ImageComp path={img} />
                         </div>
 
-                      ))}
+                        <div className="pl-4 md:pl-0 md:pt-4 my-auto md:text-center">
+                          <div className="text-4xl mb-3 font-medium">{username}</div>
+                          <div>อัตราชนะ <span className="text-primary">xx</span></div>
+                          <div>คะแนน <span className="text-primary">xx</span></div>
+                        </div>
+                      </div>
                     </div>
-                    <input type="text" className="border-black border-2 w-full rounded-lg md:w-[200px] mx-auto p-2" placeholder={username} onChange={(e) => { setEditName(e.target.value) }}></input>
-                    {feedback != "" ? <p className={`${color} bg-white py-1 rounded-md`} id="feedback-signup">{feedback}</p> : ""}
-                    <div className="grid grid-cols-2 gap-4">
-                      <button className="ring-2 ring-black rounded-lg bg-black text-white hover:bg-white hover:text-black py-2 px-6"
-                        onClick={() => saveEditProfile()}>บันทึก</button>
-                      <button className="ring-2 ring-black rounded-lg bg-black text-white hover:bg-white hover:text-black py-2 px-6"
-                        onClick={() => { setShowEdit(false); setFeedback('') }}>ยกเลิก</button>
+                    <div id="leaderboard" className="ring-2 ring-black rounded-xl bg-white p-6">
+                      <div className="text-center h-20 text-lg">เป็ดดีเด่น...</div>
                     </div>
                   </div>
-                </div>
-              </div>
-            </div> : null}
 
-          {showLogout ?
-            <div className="absolute h-full w-full flex z-20 bg-[#0005]">
-              <div className="container px-4 m-auto md:w-96">
-                <div className=" bg-white ring-2 ring-black px-4 py-8 rounded-lg grid gap-8">
-                  <div className="text-center">ออกจากระบบ</div>
-                  <div className="grid grid-cols-2 gap-8">
-                    <button className="ring-2 ring-black rounded-lg bg-white py-2 px-6"
-                      onClick={() => signOut()}>ยืนยัน</button>
-                    <button className="ring-2 ring-black rounded-lg bg-white py-2 px-6"
-                      onClick={() => { setShowLogout(false); setEditIcon(img) }}>ยกเลิก</button>
+
+                  <div className="grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-8">
+                    <button className=' rounded-lg bg-black text-white hover:bg-primary py-2 md:py-4 text-center hover:scale-105' onClick={() => { router.push('/waitingroom?intend=challenge') }}>สุ่มห้อง</button>
+                    <button className=" rounded-lg bg-black text-white hover:bg-primary py-2 md:py-4 hover:scale-105" onClick={() => { router.push('/waitingroom?intend=custom') }}>สร้างห้อง</button>
                   </div>
-                </div>
-              </div>
-            </div> : null
-          }
-
-          {invite == '' ? null :
-            <div className="absolute h-full w-full flex z-20 bg-[#0005]">
-              <div className=" m-auto bg-white ring-2 ring-black p-10 rounded-lg grid gap-8">
-                <div className="text-center">ได้รับคำเชิญ !</div>
-                <div className="grid grid-cols-2 gap-8">
-
-                  <button className="ring-2 ring-black rounded-lg bg-white py-2 px-6"
-                    onClick={() => { router.push(`/waitingroom?intend=${invite}`); removeInvite() }}>เข้าร่วม</button>
-                  <button className="ring-2 ring-black rounded-lg bg-white py-2 px-6"
-                    onClick={() => { setInvite(''); removeInvite() }}>ปฏิเสธ</button>
-
                 </div>
               </div>
             </div>
-          }
 
-          <div className="min-h-screen flex">
-            <div className="container px-6 relative z-10 my-auto mx-auto">
-              <div className="">
-                <div className="grid grid-cols-1 gap-8 mb-12 md:grid-cols-2">
-                  <div id="profile" className="ring-2 ring-black rounded-xl bg-white">
-                    <div className="px-3 py-2">
-                      <button onClick={() => { setShowEdit(true);}} className="w-5 mr-2">
-                        <ImageComp path='/image/icon/setting.svg' />
-                      </button>
-                      <button onClick={() => { setShowLogout(true) }} className="w-5">
-                        <ImageComp path='/image/icon/logout.svg' />
-                      </button>
-                    </div>
-
-                    <div className="w-full ring-black ring-1 "></div>
-
-                    <div className="grid grid-cols-2 md:grid-cols-1 p-6">
-                      <div className='md:w-2/5 md:mx-auto'>
-                        <ImageComp path={img} />
-                      </div>
-
-                      <div className="pl-4 md:pl-0 md:pt-4 my-auto md:text-center">
-                        <div className="text-4xl mb-3 font-medium">{username}</div>
-                        <div>อัตราชนะ <span className="text-primary">xx</span></div>
-                        <div>คะแนน <span className="text-primary">xx</span></div>
-                      </div>
-                    </div>
-                  </div>
-                  <div id="leaderboard" className="ring-2 ring-black rounded-xl bg-white p-6">
-                    <div className="text-center h-20 text-lg">เป็ดดีเด่น...</div>
-                  </div>
-                </div>
-
-
-                <div className="grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-8">
-                  <button className=' rounded-lg bg-black text-white hover:bg-primary py-2 md:py-4 text-center hover:scale-105' onClick={() => { router.push('/waitingroom?intend=challenge') }}>สุ่มห้อง</button>
-                  <button className=" rounded-lg bg-black text-white hover:bg-primary py-2 md:py-4 hover:scale-105" onClick={() => { router.push('/waitingroom?intend=custom') }}>สร้างห้อง</button>
-                </div>
-              </div>
-            </div>
-          </div>
-
-        </main>
-      </div>
+          </main>
+        </div>
+        :
+        <div className="h-screen w-full flex justify-center items-center">
+          <div className="p-4 text-4xl">{status}</div>
+          <span className=""><ImageComp path={'/image/icon/Spinner-0.8s-200px.gif'} /></span>
+        </div>
+      }
     </>
   );
 }
